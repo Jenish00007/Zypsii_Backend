2031f0493c82999c2f1992be89b5de668a59a9d4
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-20 10:34:40 +0530

    feat: helper function for return the place details

--- components/schedules/helpers/place.js
+++ components/schedules/helpers/place.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const axios = require('axios');
 
 //function for make the format proper
 const formatPlaceApiDetails = async (places, coordinates = undefined) => {
@@ -11,7 +12,7 @@ const formatPlaceApiDetails = async (places, coordinates = undefined) => {
 
         // function call and get the distance value in kilometer or meters
         const distanceData = calculateDistanceInKm(coordinates?.fromLatitude, coordinates?.fromLongitude, place?.geometry?.location?.lat, place?.geometry?.location?.lng);
-        
+
         return {
             _id: new mongoose.Types.ObjectId(),
             name: place.name,
@@ -63,4 +64,69 @@ const calculateDistanceInKm = (fromLatitude, fromLongitude, toLatitude, toLongit
 
 };
 
-module.exports = { formatPlaceApiDetails };
+//function return the place details fdr schedule latitude and longitude.
+const getPlaceDetail = async (userLatitude, userLongitude, fromLatitude = undefined, fromLongitude = undefined, toLatitude = undefined, toLongitude = undefined) => {
+    try {
+        let places = [];
+        let scheduleDetails = [];
+        //for get only one place details
+        if (fromLatitude && fromLongitude && !fromLatitude && !fromLongitude) {
+            const googleURL = `${process.env.GOOGLE_GEO_LOCATION_API}?latlng=${fromLatitude},${fromLongitude}&key=${process.env.GOOGLE_API_KEY}`;
+            const res = await axios.get(googleURL);
+            places.push(...res?.data?.results[0]);
+        };
+
+        //for get the two pair place details
+        if (fromLatitude && fromLongitude && toLatitude && toLongitude) {
+            const coords = [
+                [fromLatitude, fromLongitude],
+                [toLatitude, toLongitude]
+            ];
+
+            for (let i = 0; i < coords.length; i++) {
+                const [lat, lng] = coords[i];
+                const url = `${process.env.GOOGLE_GEO_LOCATION_API}?latlng=${lat},${lng}&key=${process.env.GOOGLE_API_KEY}`
+                const res = await axios.get(url);
+                // console.log('what is the data in the :res :', res.data.results)
+                places.push(res?.data?.results[0]);
+            };
+        };
+
+        // Map through places to get required fields
+        places.map(place => {
+            // function call and get the distance value in kilometer or meters
+            const distanceData = calculateDistanceInKm(userLatitude, userLongitude, place.geometry.location.lat, place.geometry.location.lng);
+            // Extract a meaningful place name
+            const addressComponents = place.address_components || [];
+
+            const localityComponent = addressComponents.find(comp => comp.types.includes("locality"));
+            const fallbackComponent = addressComponents.find(comp => comp.types.includes("administrative_area_level_3")) ||
+                addressComponents.find(comp => comp.types.includes("administrative_area_level_1"));
+
+            const placeName = localityComponent?.long_name || fallbackComponent?.long_name || place.formatted_address;
+
+            const formattedPlace = {
+                _id: new mongoose.Types.ObjectId(),
+                name: placeName,
+                address: place.formatted_address || "Address not available",
+                // image: imageUrl,
+                location: {
+                    lat: place.geometry.location.lat,
+                    lng: place.geometry.location.lng,
+                },
+                address: place.formatted_address || "Address not available",
+                distanceInKilometer: distanceData
+            };
+
+            scheduleDetails.push(formattedPlace);
+        });
+
+        return scheduleDetails;
+
+    } catch (error) {
+        console.error("Error in getPlaceDetail:", error.message);
+        throw error
+    };
+};
+
+module.exports = { formatPlaceApiDetails, getPlaceDetail };
