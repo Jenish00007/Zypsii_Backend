330205c5afece82e0bce52512335984191d540e0
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-20 16:25:12 +0530

    feat : added the rout for add description

--- components/schedules/routers/postSchedules.js
+++ components/schedules/routers/postSchedules.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateSchedule, validateJoinFields } = require('../validators/scheduleCreate');
+const { validateSchedule, validateJoinFields, validateScheduleDescription } = require('../validators/scheduleCreate');
 const upload = require('../../../middleware/multerUpload');
 const { handleValidationErrors } = require('../../../helpers/');
 const { verifyUserToken } = require('../../../middleware/users-auth');
@@ -10,4 +10,5 @@ const ScheduleController = require('../controllers/createSchedule');
 
 router.post('/create', verifyUserToken, upload.single('bannerImage'), validateSchedule, handleValidationErrors, ScheduleController.createSchedule);
 router.post('/join-un-join', verifyUserToken, validateJoinFields, handleValidationErrors, ScheduleController.joinSchedule);
+router.post('/add/descriptions/:scheduleId', verifyUserToken, validateScheduleDescription, handleValidationErrors, ScheduleController.addScheduleDescription);
 module.exports = router;
