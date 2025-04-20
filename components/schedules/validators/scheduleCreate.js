95aabae8c932773ece29dc35b3e108b062f77319
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-20 16:23:30 +0530

    feat: validation code for add description

--- components/schedules/validators/scheduleCreate.js
+++ components/schedules/validators/scheduleCreate.js
@@ -104,4 +104,30 @@ const validateJoinFields = [
         .isMongoId().withMessage('scheduleCreatedBy must be a valid MongoDB ObjectId')
 ];
 
-module.exports = { validateSchedule, validateJoinFields };
+const validateScheduleDescription = [
+    body('Description')
+        .trim()
+        .notEmpty().withMessage('Description is required'),
+
+    body('date')
+        .notEmpty().withMessage('Date is required'),
+        //.isISO8601().withMessage('Date must be in ISO8601 format (e.g., 2025-05-01T10:00:00Z)'),
+
+    body('location.from.latitude')
+        .notEmpty().withMessage('From latitude is required'),
+        //.isFloat({ min: -90, max: 90 }).withMessage('From latitude must be between -90 and 90'),
+
+    body('location.from.longitude')
+        .notEmpty().withMessage('From longitude is required')
+        .isFloat({ min: -180, max: 180 }).withMessage('From longitude must be between -180 and 180'),
+
+    body('location.to.latitude')
+        .notEmpty().withMessage('To latitude is required')
+        .isFloat({ min: -90, max: 90 }).withMessage('To latitude must be between -90 and 90'),
+
+    body('location.to.longitude')
+        .notEmpty().withMessage('To longitude is required')
+        .isFloat({ min: -180, max: 180 }).withMessage('To longitude must be between -180 and 180')
+];
+
+module.exports = { validateSchedule, validateJoinFields, validateScheduleDescription };
