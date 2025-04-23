843be0455aefa10ef23bc8bee022490622c85029
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-23 23:10:37 +0530

    feat: added the constant file for hold variables

---
+++ utils/constants.js
@@ -0,0 +1,15 @@
+// utils/constants.js
+module.exports = {
+    MEDIA_TYPES: {
+        IMAGE: 'image',
+        VIDEO: 'video',
+    },
+    POST_VISIBILITY: {
+        PUBLIC: 'Public',
+        FOLLOWERS_ONLY: 'FollowersOnly',
+    },
+    SUPPORTED_IMAGE_FORMATS: ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'],
+    SUPPORTED_VIDEO_FORMATS: ['video/mp4', 'video/mkv', 'video/webm'],
+    MAX_IMAGE_SIZE_MB: 5,
+    MAX_VIDEO_SIZE_MB: 10,
+};
