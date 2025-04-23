1a6fd766aa16991f3e82904f66aad4d4b439c90a
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-23 22:58:58 +0530

    feat: added the handleValidationErrors

--- components/follows/routes/follow.js
+++ components/follows/routes/follow.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const validateFollowParams = require('../validators/followValidation');
+const { handleValidationErrors } = require('../../../helpers/');
 
 
 const { FollowListing, FollowsManage } = require('../controllers');
 
-router.post('/followUser/:userId/:loggedInUserId', validateFollowParams, FollowsManage.createFollows);
-router.post('/unfollowUser/:userId/:loggedInUserId', validateFollowParams, FollowsManage.unFollow);
+router.post('/followUser/:userId/:loggedInUserId', validateFollowParams, handleValidationErrors, FollowsManage.createFollows);
+router.post('/unfollowUser/:userId/:loggedInUserId', validateFollowParams, handleValidationErrors, FollowsManage.unFollow);
 router.get('/getFollowers/:userId', FollowListing.getFollowers);
 router.get('/getFollowing/:userId', FollowListing.getFollowings);
 
