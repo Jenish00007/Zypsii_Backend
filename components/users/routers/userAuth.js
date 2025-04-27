db0365bbc56fd2f5bce8ad1e2a4c2bfce05a43a3
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-27 15:07:26 +0530

    feat: added the routs for otp

--- components/users/routers/userAuth.js
+++ components/users/routers/userAuth.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const { userRegistration, login } = require('../controllers/');
 const { handleValidationErrors } = require('../../../helpers/');
 const { validateUserRegistration, validateUserLogin } = require('../validators/userAuthValidation');
+const PasswordController = require('../controllers/forgotpassword');
 
 router.post('/signUp', validateUserRegistration, handleValidationErrors, userRegistration);
 router.post('/login', validateUserLogin, handleValidationErrors, login);
+router.post('/forgotPassword/getOTP/:email', PasswordController.getOTP);
+router.post('/forgotPassword/updatePassword', PasswordController.resetPassword);
 
 
 module.exports = router;
