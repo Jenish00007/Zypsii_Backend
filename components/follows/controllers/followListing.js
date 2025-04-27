b9cfaf29048a4fa8777747d5eb37e42f3c5ccd6d
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-27 11:45:51 +0530

    refactor: updated the followers listing offset and limit

--- components/follows/controllers/followListing.js
+++ components/follows/controllers/followListing.js
@@ -6,18 +6,26 @@ class FollowListing {
     // Get the followers of the logged-in user
     static async getFollowers(req, res) {
         try {
-            
+            const { limit = 10, offset = 0 } = req.query;
             const userId = new mongoose.Types.ObjectId(req.params.userId);
 
             const followersList = await follows.find({
                 following: userId,
-            }).populate('followedBy');
+            })
+                .populate('followedBy')
+                .skip(Number(offset))
+                .limit(Number(limit))
+                .sort({ createdAt: -1 });
+
+            const totalFollowersCount = await follows.countDocuments({
+                following: userId
+            });
 
             // Filter out soft-deleted users
             const filteredFollowers = followersList
                 .map(follow => follow.followedBy)
                 .filter(user => user && user.isDeleted === false)
-                .map(user =>{
+                .map(user => {
                     const userObject = user.toObject();
                     delete userObject.password;
                     return userObject;
@@ -31,7 +39,10 @@ class FollowListing {
                 status: true,
                 message: "List of followers",
                 followers: filteredFollowers,
-                followersCount: filteredFollowers.length
+                followersCount: filteredFollowers.length,
+                totalFollowers: totalFollowersCount,
+                limit: Number(limit),
+                offset: Number(offset)
             });
 
         } catch (error) {
@@ -47,11 +58,20 @@ class FollowListing {
     //Get the followings of logged-in user
     static async getFollowings(req, res) {
         try {
+            const { limit = 10, offset = 0 } = req.query;
             const userId = new mongoose.Types.ObjectId(req.params.userId);
 
             const followingList = await follows.find({
                 followedBy: userId
-            }).populate('following');
+            })
+                .populate('following')
+                .skip(Number(offset))
+                .limit(Number(limit))
+                .sort({ createdAt: -1 });
+
+            const totalFollowingCount = await follows.countDocuments({
+                followedBy: userId
+            });
 
             // Filter out soft-deleted users
             const filterFollowingList = followingList
@@ -71,7 +91,10 @@ class FollowListing {
                 status: true,
                 message: "List of following",
                 following: filterFollowingList,
-                followingCount: filterFollowingList.length
+                followingCount: filterFollowingList.length,
+                totalFollowing: totalFollowingCount,
+                limit: Number(limit),
+                offset: Number(offset)
             });
 
         } catch (error) {
