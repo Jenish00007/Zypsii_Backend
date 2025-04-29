bc7f9886803741bccbb1792cd482adcd281ca851
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-29 13:10:46 +0530

    feat: added the listing

--- components/story/routes/storyRoutes.js
+++ components/story/routes/storyRoutes.js
@@ -4,8 +4,10 @@ const { verifyUserToken } = require('../../../middleware/users-auth');
 
 //import the routers
 const storyManage = require('../controller/storyManage');
+const storyListing = require('../controller/storyListing');
 
 
 router.post('/create', verifyUserToken, storyManage.createStory);
+router.get('/list', verifyUserToken, storyListing.listStories);
 
 module.exports = router;
