5bc7967d620ea71d3e878424c1570a5a375ec073
chandra nirmal <nirmalfreelancer123@gmail.com>
2025-04-30 12:31:40 +0530

    feat: added the userLocations

--- components/users/routers/index.js
+++ components/users/routers/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // import all the routs from this folder files.
 const userAuthRoutes = require('./userAuth');
 const userProfile = require('./profile');
+const userLocations = require('./location');
 
 router.use('/user', userAuthRoutes);
 router.use('/user', userProfile);
+router.use('/user', userLocations);
 
 module.exports = router;
